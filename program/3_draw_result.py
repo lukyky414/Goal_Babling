418e8ba4639bf2805f53935485d93438c3e792a5
Lucas SCHWAB <luucka.e@gmail.com>
2020-08-31 14:36:16 +0200

    Chaques paramètres est illustré par les données correspondantes.

--- program/3_draw_result.py
+++ program/3_draw_result.py
@@ -32,8 +32,8 @@ FOLDER = "{}/{}".format(MAIN_DIR, RES_DIR)
 ##################################
 ########### FIRST STEP ###########
 ##################################
-name = "1.0mb*1000step*"
-box_1 = "1000 steps"
+name = "*fro*10res*"
+box_1 = "Resolution 10"
 
 output = subprocess.check_output("ls {}/{}".format(FOLDER, name), shell=True)
 files = output.decode('ascii')
@@ -71,8 +71,8 @@ ik_max_1 = numpy.array(tab_res1[15])
 ###################################
 ########### SECOND STEP ###########
 ###################################
-name = "1.0mb*100000step*"
-box_2 = "100 000steps"
+name = "*fro*1000res*"
+box_2 = "Reslution 1000"
 
 output = subprocess.check_output("ls {}/{}".format(FOLDER, name), shell=True)
 files = output.decode('ascii')
@@ -107,6 +107,45 @@ ik_med_2 = numpy.array(tab_res2[13])
 ik_qa3_2 = numpy.array(tab_res2[14])
 ik_max_2 = numpy.array(tab_res2[15])
 
+##################################
+########### THIRD STEP ###########
+##################################
+# name = "*fro*0.9pexp*"
+# box_3 = "Exploration 0.9"
+
+# output = subprocess.check_output("ls {}/{}".format(FOLDER, name), shell=True)
+# files = output.decode('ascii')
+
+# files = files.splitlines()
+
+# #remplissage, couver, {moy, var, mi, q1, me, q3, ma} * gl & ik
+# tab_res3 = [[], [], [], [], [], [], [], [], [], [], [], [], [], [], [], []]
+
+# for f in files:
+#     fo = open(f, "r")
+#     lines = fo.read().splitlines()
+#     for l in lines:
+#         val = l.split(" ")
+#         for i in range(16):
+#             tab_res3[i].append(float(val[i]))
+
+# rempli_3 = numpy.array(tab_res3[0])
+# couver_3 = numpy.array(tab_res3[1])
+# gl_moy_3 = numpy.array(tab_res3[2])
+# gl_var_3 = numpy.array(tab_res3[3])
+# gl_min_3 = numpy.array(tab_res3[4])
+# gl_qa1_3 = numpy.array(tab_res3[5])
+# gl_med_3 = numpy.array(tab_res3[6])
+# gl_qa3_3 = numpy.array(tab_res3[7])
+# gl_max_3 = numpy.array(tab_res3[8])
+# ik_moy_3 = numpy.array(tab_res3[9])
+# ik_var_3 = numpy.array(tab_res3[10])
+# ik_min_3 = numpy.array(tab_res3[11])
+# ik_qa1_3 = numpy.array(tab_res3[12])
+# ik_med_3 = numpy.array(tab_res3[13])
+# ik_qa3_3 = numpy.array(tab_res3[14])
+# ik_max_3 = numpy.array(tab_res3[15])
+
 
 
 #################################
@@ -119,11 +158,25 @@ plt.figure("Remplissage")
 plt.title(get_statistic_diff(rempli_1,rempli_2))
 plt.boxplot([rempli_1, rempli_2])
 plt.xticks([1, 2] , [box_1, box_2])
+# plt.title( "0.01-0.5:{} | 0.5-0.9: {} | 0.01-0.9:{}".format(
+#     get_statistic_diff(rempli_1,rempli_2),
+#     get_statistic_diff(rempli_2,rempli_3),
+#     get_statistic_diff(rempli_1,rempli_3)
+# ))
+# plt.boxplot([rempli_1, rempli_2, rempli_3])
+# plt.xticks([1, 2, 3] , [box_1, box_2, box_3])
 
 plt.figure("Couverture")
 plt.title(get_statistic_diff(couver_1,couver_2))
 plt.boxplot([couver_1, couver_2])
 plt.xticks([1, 2] , [box_1, box_2])
+# plt.title( "0.01-0.5:{} | 0.5-0.9: {} | 0.01-0.9:{}".format(
+#     get_statistic_diff(couver_1,couver_2),
+#     get_statistic_diff(couver_2,couver_3),
+#     get_statistic_diff(couver_1,couver_3)
+# ))
+# plt.boxplot([couver_1, couver_2, couver_3])
+# plt.xticks([1, 2, 3] , [box_1, box_2, box_3])
 
 
 
@@ -131,6 +184,13 @@ plt.figure("Moyenne Goals")
 plt.title(get_statistic_diff(gl_moy_1,gl_moy_2))
 plt.boxplot([gl_moy_1, gl_moy_2])
 plt.xticks([1, 2] , [box_1, box_2])
+# plt.title( "0.01-0.5:{} | 0.5-0.9: {} | 0.01-0.9:{}".format(
+#     get_statistic_diff(gl_moy_1,gl_moy_2),
+#     get_statistic_diff(gl_moy_2,gl_moy_3),
+#     get_statistic_diff(gl_moy_1,gl_moy_3)
+# ))
+# plt.boxplot([gl_moy_1, gl_moy_2, gl_moy_3])
+# plt.xticks([1, 2, 3] , [box_1, box_2, box_3])
 
 # plt.figure("Variance Goals")
 # plt.title(get_statistic_diff(gl_var_1,gl_var_2))
@@ -168,6 +228,13 @@ plt.figure("Moyenne Ikpy")
 plt.title(get_statistic_diff(ik_moy_1,ik_moy_2))
 plt.boxplot([ik_moy_1, ik_moy_2])
 plt.xticks([1, 2] , [box_1, box_2])
+# plt.title( "0.01-0.5:{} | 0.5-0.9: {} | 0.01-0.9:{}".format(
+#     get_statistic_diff(ik_moy_1,ik_moy_2),
+#     get_statistic_diff(ik_moy_2,ik_moy_3),
+#     get_statistic_diff(ik_moy_1,ik_moy_3)
+# ))
+# plt.boxplot([ik_moy_1, ik_moy_2, ik_moy_3])
+# plt.xticks([1, 2, 3] , [box_1, box_2, box_3])
 
 # plt.figure("Variance Ikpy")
 # plt.title(get_statistic_diff(ik_var_1,ik_var_2))
